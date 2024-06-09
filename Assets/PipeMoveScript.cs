81eaa0c8940bf88c9dcb4ab3fd6f718519af4768
Hogan Wong <hoganwong88@gmail.com>
2024-06-09 16:43:50 +0800

    done i guess

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour {
     public float moveSpeed = 5f;
-    public float deadZone = -45;
 
     // Start is called before the first frame update
     void Start() {
@@ -13,8 +12,6 @@ public class PipeMoveScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (transform.position.x > deadZone) {
-            Destroy(gameObject);
-        }
+        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
     }
 }
